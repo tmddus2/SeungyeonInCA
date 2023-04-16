05e170ab4b83f7d31ccf978ef1d8ff9f51b63ab0
tmddus2 <tmddus2__@naver.com>
2023-04-16 00:44:50 -0700

    fix: S3 이미지 업로드 수정

--- src/main/java/com/hello/basic/service/S3Uploader.java
+++ src/main/java/com/hello/basic/service/S3Uploader.java
@@ -1,2 +1,58 @@
-package com.hello.basic.service;public class S3Uploader {
+package com.hello.basic.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class S3Uploader {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+        String fileName = UUID.randomUUID() + "_" + multipartFile.getOriginalFilename();
+        File uploadFile = new File(System.getProperty("user.home"), fileName) ;
+        multipartFile.transferTo(uploadFile);
+        log.info(uploadFile.getName());
+        return upload(uploadFile, dirName);
+    }
+
+    private String upload(File uploadFile, String dirName) {
+        String newName = dirName + "/" + uploadFile.getName();
+        String uploadImageUrl = putS3(uploadFile, newName);
+        removeNewFile(uploadFile);
+        return uploadImageUrl;
+    }
+
+    private String putS3(File uploadFile, String fileName) {
+        amazonS3Client.putObject(
+                new PutObjectRequest(bucket, fileName, uploadFile)
+                        .withCannedAcl(CannedAccessControlList.PublicRead)
+        );
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+
+    private void removeNewFile(File targetFile) {
+        if(targetFile.delete()) {
+            log.info("파일이 삭제되었습니다.");
+        }else {
+            log.info("파일이 삭제되지 못했습니다.");
+        }
+    }
+
 }
