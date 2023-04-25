8b23ade30e250ce085aa0b4c80f16af381ffba93
tmddus2 <tmddus2__@naver.com>
2023-04-25 00:18:27 -0700

    feat: 회원가입 및 로그인 form 데이터 전달

--- src/main/java/com/hello/basic/web/basic/UserController.java
+++ src/main/java/com/hello/basic/web/basic/UserController.java
@@ -1,8 +1,13 @@
 package com.hello.basic.web.basic;
 
+import com.hello.basic.dto.SignInDto;
+import com.hello.basic.dto.SignUpDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,12 +16,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @GetMapping("/sign-in")
-    public String signIn(){
+    public String signIn(Model model){
+        model.addAttribute("signInDto", new SignInDto());
         return "userPage/signIn";
     }
 
     @GetMapping("/sign-up")
-    public String signUp(){
+    public String signUp(Model model){
+        model.addAttribute("signUpDto", new SignUpDto());
         return "userPage/signUp";
+    }
+
+    @PostMapping("/sign-up")
+    public String registerUser(@ModelAttribute("signUpDto") SignUpDto signUpDto) {
+        System.out.println("signUpDto = " + signUpDto.toString());
+        return "redirect:/sign-in";
+    }
+
+    @PostMapping("/sign-in")
+    public String login(@ModelAttribute("signInDto") SignInDto signInDto) {
+        System.out.println("signInDto = " + signInDto);
+        return "redirect:/";
     }
 }
