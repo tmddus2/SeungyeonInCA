b114dfde369bfcb147efaf10951e2a1d9e6ebb5c
tmddus2 <tmddus2__@naver.com>
2023-06-14 00:01:38 -0700

    feat: user entity 수정

--- src/main/java/com/hello/basic/entity/User.java
+++ src/main/java/com/hello/basic/entity/User.java
@@ -1,10 +1,19 @@
 package com.hello.basic.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
